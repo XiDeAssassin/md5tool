4542cb33d397da88f6d89db126543d351e02e7cd
XinoAssassin Ni <phnjs211@outlook.com>
2017-02-28 01:01:41 +0800

    new project

---
+++ md5tool/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace md5tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Func obj = new Func();
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                InitialDirectory = Environment.SpecialFolder.Desktop.ToString()
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("MD5 is " + obj.MD5file(dialog.FileName) + Environment.NewLine + "SHA-1 is " + obj.SHA1file(dialog.FileName));
+            }
+        }
+    }
+}
---
+++ md5tool/Func.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace md5tool
+{
+    class Func
+    {
+        public string MD5file(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty);
+                }
+            }
+        }
+
+        public string SHA1file(string filename)
+        {
+            using (var sha1 = SHA1.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    return BitConverter.ToString(sha1.ComputeHash(stream)).Replace("-", string.Empty);
+                }
+            }
+        }
+    }
+}
